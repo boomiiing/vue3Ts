8ebe6bef71b420e8f5df51d34299d9f16bff11c9
HOTLEE_home <1215574240@qq.com>
2024-07-15 22:19:27 +0800

    vue3的初始化

---
+++ hello_vue3/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="app">hello nihao</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+name:'App'
+}
+</script>
+
+<style>
+ .app{
+    background-color: #094;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+ }
+</style>
