91c5f80d4d3e70cbb48ef88fb72e940f10cd973f
HOTLEE_home <1215574240@qq.com>
2024-07-22 21:27:32 +0800

    vue3中props与生命周期的学习

---
+++ hello_vue3/备份/021_props/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Person ref="person" :persons = 'persons'></Person>
+  </div>
+</template>
+
+<script lang="ts" setup name="App">
+import { reactive } from 'vue';
+import Person from './components/Person.vue'
+import {type personInterface} from './Types'
+
+let persons = reactive([
+    {
+        name:'张三',
+    age:12,
+    id:'3123',
+    },
+    {
+        name:'李四',
+    age:12,
+    id:'3123',
+    },
+    {
+        name:'王五',
+    age:12,
+    id:'3123',
+    }
+])
+</script>
+
+<style>
+
+</style>
---
+++ hello_vue3/备份/021_props/components/Person.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="person">
+        <ul v-for="(item,index) in person.persons" :key="index">
+            <li v-for="(ele,indexs) in item" :key="indexs">{{ ele }}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang='ts' setup  name="Person">
+import { defineProps, withDefaults} from 'vue';
+import { type personInterface } from '../Types';
+//只接受list
+// let person = defineProps(['persons'])
+//接受list + 限制类型
+// let person = defineProps<{persons:Array<personInterface>}>()
+//接受list + 限制类型 + 默认值
+let person =  withDefaults(defineProps<{persons:Array<personInterface>}>(),{
+    persons:() => [ {
+        name:'ll',
+        age:26,
+        id:'08',
+        },]
+})
+
+</script>
+<style scoped >
+.person{
+    background-color: #ccc;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style> 
